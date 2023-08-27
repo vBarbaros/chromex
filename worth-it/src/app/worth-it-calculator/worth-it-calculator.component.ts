c07782a00a612df104c5c1708da6b629c44d33c8
vBarbaros <victor.barbarosh@gmail.com>
2023-08-27 09:20:48 -0400

    Define the worth-it calculator

---
+++ worth-it/src/app/worth-it-calculator/worth-it-calculator.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+
+  selector: 'app-worth-it-calculator',
+  templateUrl: './worth-it-calculator.component.html',
+  styleUrls: ['./worth-it-calculator.component.css']
+})
+export class WorthItCalculatorComponent implements OnInit {
+
+  myForm: FormGroup;
+  referenceValue: FormControl;
+  referenceName: FormControl | undefined;
+  newItemValue: FormControl | undefined;
+  newItemName: FormControl | undefined;
+  conclusionIsReady: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = fb.group({
+      'referenceValue': [null, Validators.nullValidator],
+      'referenceName': ['', Validators.nullValidator],
+      'newItemValue': [null, Validators.nullValidator],
+      'newItemName': ['',Validators.nullValidator ],
+    });
+    this.referenceValue = this.myForm.controls['referenceValue'] as FormControl;
+  }
+
+  ngOnInit() {
+    // @ts-ignore
+    this.referenceValue.valueChanges.subscribe(value => {
+      // Make sure the control exists before using it
+      this.referenceName = value.referenceName;
+      this.referenceValue = value.referenceValue;
+      this.newItemName = value.newItemName;
+      this.newItemValue = value.newItemValue;
+    });
+  }
+
+  onSubmit(form: any): void {
+    this.referenceName = form.referenceName.value;
+    this.referenceValue = form.referenceValue.value;
+    this.newItemName = form.newItemName.value;
+    this.newItemValue = form.newItemValue.value;
+  }
+
+  resetForm() {
+    this.myForm.reset();
+  }
+
+  calculateQuantity(): String {
+    const referenceValue = this.myForm.value.referenceValue;
+    const newItemValue = this.myForm.value.newItemValue;
+    let finalWorthItMessage = 'By choosing differently, you could afford  ';
+    if (referenceValue >= newItemValue && referenceValue > 0 && newItemValue > 0) {
+      finalWorthItMessage += this.formatDecimal(referenceValue / newItemValue);
+    } else if (referenceValue < newItemValue && referenceValue > 0 && newItemValue > 0) {
+      finalWorthItMessage += this.formatDecimal(newItemValue / referenceValue);
+    }
+    finalWorthItMessage += ' ' + this.myForm.value.referenceName + '(s) instead of just one ' + this.myForm.value.newItemName;
+    finalWorthItMessage += ' illustrating the substantial opportunity cost you gain.';
+    return finalWorthItMessage;
+  }
+
+  formatDecimal(decimalNumber: number): string {
+    // Use the toFixed method to format the number to two decimal places
+    return decimalNumber.toFixed(2);
+  }
+
+}
